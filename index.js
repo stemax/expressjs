5cd70020501acc43a05501e492c84531b42ce468
stemax <max01oe@yandex.ru>
2022-11-14 15:14:02 +0300

    Routing update(part1)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import {requestTime, logger} from './middlewares.js'
+import serverRoutes from "./routes/servers.js";
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3001
@@ -9,6 +10,12 @@ const app = express()
 app.set('view engine', 'ejs')
 // console.log(app.get('view engine'))
 // console.log(app.get('views'))
+
+//Middlewares
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
+app.use(serverRoutes)
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Main page', active: 'main'})
@@ -21,10 +28,6 @@ app.get('/features', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {title:'About', active:'about'})
 })
-
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(requestTime)
-app.use(logger)
 // app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 // })
